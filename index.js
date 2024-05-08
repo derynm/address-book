21f462e19df39d4521b3cd0d82e5768a9fabcb7b
derynm <dery.marjuki@gli.id>
2024-05-08 10:47:17 +0700

    refactor: search function

--- index.js
+++ index.js
@@ -1,55 +1,51 @@
 function getContactFromStorage() {
-  return JSON.parse(localStorage.getItem('contact'))
+  return JSON.parse(localStorage.getItem('contact'));
 }
 
 function saveContactToStorage(contact) {
-  localStorage.setItem('contact', JSON.stringify(contact))
+  localStorage.setItem('contact', JSON.stringify(contact));
 }
 
 window.addEventListener('load', function () {
   if (getContactFromStorage()) {
-    renderContactCard()
+    renderContactCard();
   }
-})
+});
 
-const contactForm = document.getElementById('add-contact-form')
+function searchContact(key) {
+  return getContactFromStorage().filter((contact) =>
+    contact.name.toLowerCase().includes(key.toLowerCase())
+  );
+}
 
-contactForm.addEventListener('submit', saveContact)
+const contactForm = document.getElementById('add-contact-form');
+
+contactForm.addEventListener('submit', saveContact);
 
 function saveContact(e) {
-  e.preventDefault()
-  const currentContact = getContactFromStorage()
-  const contactFormData = new FormData(contactForm) 
-  const newContact = Object.fromEntries(contactFormData)
+  e.preventDefault();
+  const currentContact = getContactFromStorage();
+  const contactFormData = new FormData(contactForm);
+  const newContact = Object.fromEntries(contactFormData);
   const contact = currentContact
     ? [...currentContact, newContact]
-    : [newContact]
+    : [newContact];
 
-  saveContactToStorage(contact)
-  renderContactCard()
-  contactForm.reset()
+  saveContactToStorage(contact);
+  renderContactCard();
+  contactForm.reset();
 }
 
-const searchContactForm = document.getElementById('search-form')
+function renderContactCard() {
+  const searchKey = new URLSearchParams(window.location.search).get('search');
+  const contact = searchKey
+    ? searchContact(searchKey)
+    : getContactFromStorage();
 
-searchContactForm.addEventListener('submit', searchContact)
-
-function searchContact(e) {
-  e.preventDefault()
-  const searchValue = document.getElementById('search-value').value
-  renderContactCard(
-    getContactFromStorage().filter((contact) =>
-      contact.name.toLowerCase().includes(searchValue.toLowerCase())
-    )
-  )
-}
-
-function renderContactCard(data) {
-  const contact = data ?? getContactFromStorage()
-  const contactCard = document.getElementById('contact-container')
-  contactCard.innerHTML = ''
+  const contactCard = document.getElementById('contact-container');
+  contactCard.innerHTML = '';
   contact.forEach((contact) => {
-    const card = document.createElement('div')
+    const card = document.createElement('div');
     card.innerHTML = `
     <div class="bg-white shadow-md border slate-300 rounded-md p-4">
       <h3 class="text-xl font-semibold">${contact.name}</h3>
@@ -57,7 +53,7 @@ function renderContactCard(data) {
       <p>${contact.email}</p>
       <p>${contact.address}</p>
     </div>
-    `
-    contactCard.appendChild(card)
-  })
+    `;
+    contactCard.appendChild(card);
+  });
 }
